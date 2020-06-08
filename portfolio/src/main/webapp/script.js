2cca78bf68086afbe35700389527f7004a9b3c41
Shawn Jackson <Shawn Jackson>
2020-06-08 19:52:10 +0000

    Create map page and inital call

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -174,3 +174,10 @@ async function siteEvaluationData() {
 
     return [perfectData, servicabledata, improvementData];
 }
+
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
+
